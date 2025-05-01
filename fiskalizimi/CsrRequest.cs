bcaf823ef186ce761874534de2fa14d8de3cb260
Gazmend Mehmeti <gazmendmehmeti@gmail.com>
2025-05-01 09:09:00 +0200

    Refactor PKI entity for better structure

---
+++ fiskalizimi/CsrRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fiskalizimi
+{
+    public class CsrRequest
+    {
+        public string Country { get; set; }
+        public string BusinessName { get; set; }
+        public ulong Nui { get; set; }
+        public ulong BranchId { get; set; }
+        public ulong PosId { get; set; }
+    }
+
+}
